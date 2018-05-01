42f99c16fbdeed28d7fea816585548b43521f4fa
Alex Moisescu <moisescu.alexandru88@gmail.com>
2018-05-01 13:42:04 +0300

    Instantiated the objects (Enemies and Player)

--- js/app.js
+++ js/app.js
@@ -97,7 +97,11 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+const enemy1 = new Enemy;
+const enemy2 = new Enemy;
+const enemy3 = new Enemy;
+var allEnemies = [enemy1, enemy2, enemy3];
+const player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
